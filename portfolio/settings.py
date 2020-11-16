36276de83a6df0b09f30a5788dcdcb5f62cd0325
Agozie Favour <fagozie43@gmail.com>
2020-11-16 02:11:30 +0100

    Changed template and static path

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,9 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = BASE_DIR / 'templates'
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
