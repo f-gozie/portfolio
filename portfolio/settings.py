6d9f762812cf1ee5f7f4819a0bd799815aa1a93f
Agozie Favour <fagozie43@gmail.com>
2020-11-19 21:48:19 +0100

    Fix: Static file referencing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static/'
-# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 django_heroku.settings(locals())
