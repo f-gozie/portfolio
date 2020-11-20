79414e0ce193932313c6ba53cb96da830848d9a3
Agozie Favour <fagozie43@gmail.com>
2020-11-20 01:05:35 +0100

    Added views, and models

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from projects import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('projects', views.overview, name='overview'),
+	path('projects/<int:pk>/', views.detail, name='detail')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def index(request):
 	return render(request, "portfolio/index.html")
+
+def overview(request):
+	projects = Project.objects.all()
+	context = {
+		'projects':projects
+	}
+	return render(request, "portfolio/overview.html", context)
+
+def detail(request, pk):
+	project = Project.objects.get(pk=pk)
+	context = {
+		'project':project
+	}
+	return render(request, "portfolio/detail.html", context)
