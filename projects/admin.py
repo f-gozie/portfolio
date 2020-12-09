2b91f42d5f26fa9f8e4141bebaa47ff4033f00f4
Agozie Favour <fagozie43@gmail.com>
2020-12-09 21:57:14 +0100

    Chore: Updated admin contact list page

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Project, Contact
 
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email', 'message']
+
 # Register your models here.
 admin.site.register(Project)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
